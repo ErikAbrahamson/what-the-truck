92b565045312e1aa4530c0df318f67be8d461988
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-12 12:37:17 -0600

    get venues from ajax request

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 
 var search = 'https://api.foursquare.com/v2/venues/explore?';
 var ID = '&client_id=L315SVFDTIGOFB4XCGNPEKM2S5CHEO24T4YPEMBTLP2UP3ZP&client_secret=GX2APOUMZCXT4DHUS4BZZKVNDTMMBSZFKMQM4LK1II3JOJOE';
-// https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
-// search?request
-// intent?browse
-// radius? (in meters)
 
 // $(document).ready(function() {
   //set intent to browse
@@ -26,14 +22,18 @@ var ID = '&client_id=L315SVFDTIGOFB4XCGNPEKM2S5CHEO24T4YPEMBTLP2UP3ZP&client_sec
     return today;
   };
 
-  //AJAX Request
+  //AJAX Request to get venue list
   $.ajax({
     url: search + denver + '&query=Food%20Truck' + ID + currentDate(),
     type: 'GET',
     dataType: 'JSON',
-    data: [{ client_id:'L315SVFDTIGOFB4XCGNPEKM2S5CHEO24T4YPEMBTLP2UP3ZP'},{ client_secret:'GX2APOUMZCXT4DHUS4BZZKVNDTMMBSZFKMQM4LK1II3JOJOE'}],
+    data: [
+      { client_id:'L315SVFDTIGOFB4XCGNPEKM2S5CHEO24T4YPEMBTLP2UP3ZP'},
+      { client_secret:'GX2APOUMZCXT4DHUS4BZZKVNDTMMBSZFKMQM4LK1II3JOJOE'}],
     success: function(data) {
-      console.log(data);
+      console.log(data.response.groups[0].items);
+      venues = data.response.groups[0].items;
+      return venues;
     },
     error: function(data) {
       console.log('error');
