beb3d992f58ba4073142dac5af6a0e1cf3dd0b29
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-13 11:32:13 -0600

    continue to implement functional programming

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,28 @@
-
+var x = {
+  sortHigh: function() {
+    var sorted = [];
+      for (var i = 0; i < currentList.length; i++) {
+        if (currentList[i].venue.rating > 5 && currentList[i].venue.rating !== undefined) {
+          currentList.sort(function(a, b) {
+            return b.venue.rating - a.venue.rating;
+          });
+          sorted.push(currentList[i]);
+        }
+      }
+      return sorted;
+    },
+  sortLow: function() {
+    var sorted = [];
+      for (var i = 0; i < currentList.length; i++) {
+        if (currentList[i].venue.rating < 5 || currentList[i].venue.rating === undefined) {
+          currentList.sort(function(a, b) {
+            return a.venue.rating - b.venue.rating;
+          });
+        sorted.push(currentList[i]);
+      }
+    }
+    return sorted;
+  }
+};
 
 var currentList = [];
