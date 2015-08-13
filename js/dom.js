baa446e3d64cdbce5df7cac9d5bfeb83d1f39d51
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-13 14:41:11 -0600

    More ajax request refining

--- js/dom.js
+++ js/dom.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
   setTimeout(function() {
     $.ajax({
-      url: 'https://api.foursquare.com/v2/venues/search',
+      url: 'https://api.foursquare.com/v2/venues/explore',
       type: 'GET',
       data: {
+        ll: '39.733848,-104.992459', // can make dynamic
+        query: 'Food Truck', // can make dynamic. lol
         client_id: 'L315SVFDTIGOFB4XCGNPEKM2S5CHEO24T4YPEMBTLP2UP3ZP',
         client_secret: 'GX2APOUMZCXT4DHUS4BZZKVNDTMMBSZFKMQM4LK1II3JOJOE',
-        v: get.currentDate(),
-        ll: '39.733848,-104.992459', // can make dynamic
-        query: 'Food Truck' // can make dynamic
+        v: get.currentDate()
       },
       dataType: 'json',
       success: function(data) {
-        console.log(data.response.venues);
-        venues = data.response.venues;
+        console.log(data);
+        venues = data.response.groups[0].items;
         get.initVenues(venues);
+      },
+      error: function() {
+        console.log('There was a problem with the request');
       }
     });
-  }, 500);
+  }, 300);
+  // DOM stuff here
 });
-
-// https://api.foursquare.com/v2/venues/search?client_id=L315SVFDTIGOFB4XCGNPEKM2S5CHEO24T4YPEMBTLP2UP3ZP&client_secret=GX2APOUMZCXT4DHUS4BZZKVNDTMMBSZFKMQM4LK1II3JOJOE&v=20150813&ll=39.733848%2C2C104.992459&query=Food+Truck
