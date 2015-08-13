6b3ebe2ae74ec6f188e46d641636525eb31de52b
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-13 11:25:56 -0600

    Begin making logic file for functions

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
   setTimeout(function() {
+    $('#sort-rating').on('click', function(event) {
+      event.preventDefault();
+
+      function sortHigh() {
+        var sorted = [];
+        for (var i = 0; i < currentList.length; i++) {
+          if (currentList[i].venue.rating > 8 && currentList[i].venue.rating !== undefined) {
+            currentList.sort(function(a, b) {
+              return b.venue.rating - a.venue.rating;
+            });
+            sorted.push(currentList[i]);
+            }
+          }
+        return sorted;
+        }
+
+
+    });
     console.log(currentList);
-  }, 500);
+  }, 1000);
 });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-var currentList = [];
 $(document).ready(function() {
-
   var search = 'https://api.foursquare.com/v2/venues/explore?';
   var ID = '&client_id=L315SVFDTIGOFB4XCGNPEKM2S5CHEO24T4YPEMBTLP2UP3ZP&client_secret=GX2APOUMZCXT4DHUS4BZZKVNDTMMBSZFKMQM4LK1II3JOJOE';
   var denver = 'll=39.733848%2C-104.992459';
