535b81268a2e72c57b6fe578de37b6364638aaf6
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-02 15:56:26 -0600

    Minor CSS fixes

--- js/utility.js
+++ js/utility.js
@@ -7,12 +7,10 @@ var get = {
       url: 'https://api.foursquare.com/v2/venues/explore',
       type: 'GET',
          data: {
-         // ll: '39.7,-104.9',
             ll: ll,
             query: 'Food Truck',
             venuePhotos: 1,
             sortByDistance: 1,
-            // radius: 40000,
             client_id: 'L315SVFDTIGOFB4XCGNPEKM2S5CHEO24T4YPEMBTLP2UP3ZP',
             client_secret: 'GX2APOUMZCXT4DHUS4BZZKVNDTMMBSZFKMQM4LK1II3JOJOE',
             v: get.currentDate()
